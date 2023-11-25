d10799a0387073efcfb3eb51eeaa365a0fa49604
0118Add <60157610+0118Add@users.noreply.github.com>
2023-11-25 08:44:40 +0800

    Update diy-x86.sh

--- diy-x86.sh
+++ diy-x86.sh
@@ -90,7 +90,7 @@ rm -rf feeds/luci/applications/luci-app-serverchan
 #git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 #svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-dockerman package/luci-app-dockerman
 svn co https://github.com/kiddin9/openwrt-packages/trunk/lua-maxminddb package/lua-maxminddb
-svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-vssr package/luci-app-vssr
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vssr package/luci-app-vssr
 #svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
