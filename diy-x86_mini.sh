04e6ad50b3b82f877ee4196ea5ebcac8adc58230
0118Add <60157610+0118Add@users.noreply.github.com>
2024-01-28 12:59:04 +0800

    Update diy-x86_mini.sh

--- diy-x86_mini.sh
+++ diy-x86_mini.sh
@@ -101,7 +101,7 @@ git clone https://github.com/0118Add/luci-app-vssr package/luci-app-vssr
 #svn co https://github.com/0118Add/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
-git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
+#git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
 git clone https://github.com/fw876/helloworld.git package/helloworld
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/passwall2
 #git clone https://github.com/sbwml/luci-app-alist.git package/alist
