5a510aa9f6f16994f85dfc62e8b8b4fc4cef816b
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-06 14:19:31 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,62 +1,41 @@
 #!/bin/bash
-# smartdns
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git package/new/luci-app-smartdns
+rm -rf feeds/packages/net/{xray-core,sing-box,v2ray-geodata}
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 
-# fanchmwrt
-# 1. 克隆 fanchmwrt 仓库到临时目录 (使用 --depth 1 减少下载量)
-git clone --depth 1 https://github.com/fanchmwrt/fanchmwrt.git /tmp/fanchmwrt
-# 2. 复制 fcm
-cp -r /tmp/fanchmwrt/package/fcm package/
-# 3. 清理临时文件
-rm -rf /tmp/fanchmwrt
-# 4. (可选) 修正权限，防止脚本没有执行权限
-chmod -R 755 package/fcm
-# patch
-curl -s https://raw.githubusercontent.com/fanchmwrt/fanchmwrt/fanchmwrt-24.10.4/target/linux/generic/hack-6.6/980-nf-contrack-support-fwx-data.patch > target/linux/generic/pending-6.12/999-fwx-kernel-hook.patch
-# fanchmwrt-packages
-git clone https://github.com/gitbruc/fanchmwrt-packages package/new/fanchmwrt-packages
+# OpenClash
+git clone -b dev --depth 1 https://github.com/vernesong/OpenClash package/OpenClash
 
-# eqosplus
-git clone https://github.com/sirpdboy/luci-app-eqosplus.git package/new/luci-app-eqosplus
-# wrtbwmon
-#git clone https://github.com/gitbruc/openwrt-wrtbwmon.git package/new/luci-app-wrtbwmon
-# onliner
-git clone https://github.com/gitbruc/luci-onliner.git package/new/luci-app-onliner
-# ddns-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go package/new/ddnsgo
-# wechatpush
-git clone https://github.com/tty228/luci-app-wechatpush.git package/new/luci-app-wechatpush
-# bbrswitch
-git clone https://github.com/gitbruc/openwrt-BBR.git package/new/luci-app-bbrswitch
-# passwall
-rm -rf package/new/helloworld/{luci-app-passwall,patch-luci-app-passwall.patch}
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall.git package/new/helloworld/luci-app-passwall
-# luci-app-taskplan
-git clone https://github.com/sirpdboy/luci-app-taskplan.git package/new/luci-app-taskplan
-# control-sqm
-sed -i 's|admin/network|admin/control|' "feeds/luci/applications/luci-app-sqm/root/usr/share/luci/menu.d/luci-app-sqm.json"
-# control-nlbw
-sed -i 's|admin/services|admin/control|g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
-sed -i 's|admin/services|admin/control|g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
-# control-banip
-sed -i 's|admin/services|admin/control|' "feeds/luci/applications/luci-app-banip/root/usr/share/luci/menu.d/luci-app-banip.json"
-# control-watchcat
-sed -i 's|admin/services|admin/control|' "feeds/luci/applications/luci-app-watchcat/root/usr/share/luci/menu.d/luci-app-watchcat.json"
-# control-wol
-sed -i 's|admin/services|admin/control|' "feeds/luci/applications/luci-app-wol/root/usr/share/luci/menu.d/luci-app-wol.json"
-# control-openappfilter---
-sed -i 's|"admin", "services"|"admin", "control"|g' "package/new/OpenAppFilter/luci-app-oaf/luasrc/controller/appfilter.lua"
-sed -i 's|"admin", "services"|"admin", "control"|g' "package/new/OpenAppFilter/luci-app-oaf/luasrc/model/cbi/appfilter/dev_status.lua"
-sed -i 's|admin/services|admin/control|g' "package/new/OpenAppFilter/luci-app-oaf/luasrc/view/admin_network/app_filter.htm"
-# nas-samba4
-sed -i 's|admin/services|admin/nas|' "feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json"
-# nas-webdav
-sed -i 's|admin/services/webdav|admin/nas/webdav|g' package/new/luci-app-webdav/root/usr/share/luci/menu.d/luci-app-webdav.json
+# homeproxy
+#git clone -b dev --depth 1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git clone --depth 1 -b master https://github.com/fun200/homeproxy_plus package/luci-app-homeproxy
+sed -i "s/ImmortalWrt/OpenWrt/g" package/luci-app-homeproxy/po/zh_Hans/homeproxy.po
+sed -i "s/ImmortalWrt proxy/OpenWrt proxy/g" package/luci-app-homeproxy/htdocs/luci-static/resources/view/homeproxy/{client.js,server.js}
+
+# dae daed
+#git clone https://github.com/kenzok8/openwrt-daede package/daede
+#git clone -b kix --depth 1 https://github.com/QiuSimons/luci-app-dae package/dae
+git clone -b kix --depth 1 https://github.com/QiuSimons/luci-app-daed package/daed
+git clone https://github.com/QiuSimons/vmlinux-btf package/vmlinux-btf
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=2026.06.14/g' package/daed/daed/Makefile
+sed -i 's/DAED_VERSION:=.*/DAED_VERSION:=daed-4d6a433/g' package/daed/daed/Makefile
+sed -i 's/WING_VERSION:=.*/WING_VERSION:=wing-dc50308/g' package/daed/daed/Makefile
+sed -i 's/CORE_VERSION:=.*/CORE_VERSION:=core-5a51cc7/g' package/daed/daed/Makefile
+sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:=4d6a43331f2f6e25961935b9e7ac09a7568bb2b4/g' package/daed/daed/Makefile
+
+# 克隆immortalwrt-luci packages仓库
+git clone --depth=1 -b openwrt-25.12 https://github.com/immortalwrt/luci.git immortalwrt-luci
+cp -rf immortalwrt-luci/applications/luci-app-diskman feeds/luci/applications/luci-app-diskman
+ln -sf ../../../feeds/luci/applications/luci-app-diskman ./package/feeds/luci/luci-app-diskman
+cp -rf immortalwrt-luci/applications/luci-app-msd_lite feeds/luci/applications/luci-app-msd_lite
+ln -sf ../../../feeds/luci/applications/luci-app-msd_lite ./package/feeds/luci/luci-app-msd_lite
+cp -rf immortalwrt-luci/applications/luci-app-ramfree feeds/luci/applications/luci-app-ramfree
+ln -sf ../../../feeds/luci/applications/luci-app-ramfree ./package/feeds/luci/luci-app-ramfree
+git clone --depth=1 -b openwrt-25.12 https://github.com/immortalwrt/packages.git immortalwrt-packages
+cp -rf immortalwrt-packages/net/msd_lite feeds/packages/net/msd_lite
+ln -sf ../../../feeds/packages/net/msd_lite ./package/feeds/packages/msd_lite
+
 # change luci
-perl -pi -e "s/hostname='OpenWrt'/hostname='XiaomanWrt'/g" package/base-files/files/bin/config_generate
-node insert.js
-mv 1.png package/new/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/background/
+#perl -pi -e "s/hostname='OpenWrt'/hostname='XiaomanWrt'/g" package/base-files/files/bin/config_generate
+#node insert.js
+#mv 1.png package/new/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/background/
 # 自定义脚本
