a5d9e2df896170fd358d7c4344ab73efe8a6430a
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-08 22:33:04 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -401,23 +401,23 @@ fi
 
 if [ "$platform" = "x86_64" ]; then
     if [ "$NO_KMOD" != "y" ]; then
-        cp -a bin/targets/x86/*/packages $kmodpkg_name
-        rm -f $kmodpkg_name/Packages*
-        cp -a bin/packages/x86_64/base/rtl88*a-firmware*.apk $kmodpkg_name/ || true
-        cp -a bin/packages/x86_64/base/natflow*.apk $kmodpkg_name/ || true
+        cp -a bin/targets/x86/*/packages kmodpkg
+        rm -f kmodpkg/Packages*
+        cp -a bin/packages/x86_64/base/rtl88*a-firmware*.apk kmodpkg/ || true
+        cp -a bin/packages/x86_64/base/natflow*.apk kmodpkg/ || true
         [ "$OPENWRT_CORE" = "y" ] && {
-            cp -a bin/packages/x86_64/base/*3ginfo*.apk $kmodpkg_name/ || true
-            cp -a bin/packages/x86_64/base/*modemband*.apk $kmodpkg_name/ || true
-            cp -a bin/packages/x86_64/base/*sms-tool*.apk $kmodpkg_name/ || true
-            cp -a bin/packages/x86_64/base/*quectel*.apk $kmodpkg_name/ || true
+            cp -a bin/packages/x86_64/base/*3ginfo*.apk kmodpkg/ || true
+            cp -a bin/packages/x86_64/base/*modemband*.apk kmodpkg/ || true
+            cp -a bin/packages/x86_64/base/*sms-tool*.apk kmodpkg/ || true
+            cp -a bin/packages/x86_64/base/*quectel*.apk kmodpkg/ || true
         }
         [ "$ENABLE_DPDK" = "y" ] && {
-            cp -a bin/packages/x86_64/base/*dpdk*.apk $kmodpkg_name/ || true
-            cp -a bin/packages/x86_64/base/*numa*.apk $kmodpkg_name/ || true
+            cp -a bin/packages/x86_64/base/*dpdk*.apk kmodpkg/ || true
+            cp -a bin/packages/x86_64/base/*numa*.apk kmodpkg/ || true
         }
-        bash kmod-sign $kmodpkg_name
-        tar zcf x86_64-$kmodpkg.tar.gz $kmodpkg_name
-        rm -rf $kmodpkg_name
+        bash kmod-sign kmodpkg
+        tar zcf x86_64-kmodpkg.tar.gz kmodpkg
+        rm -rf kmodpkg
     fi
     # Backup download cache
     if [ "$isCN" = "CN" ] && [ "$1" = "rc2" ]; then
