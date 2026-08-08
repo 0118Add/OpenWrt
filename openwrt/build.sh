cb6c8c9d1a5e42214294779151cb05be4e5dfa80
0118Add <60157610+0118Add@users.noreply.github.com>
2026-08-08 21:42:16 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -401,7 +401,7 @@ fi
 
 if [ "$platform" = "x86_64" ]; then
     if [ "$NO_KMOD" != "y" ]; then
-        cp -a bin/targets/x86/*/packages kmodpkg_name
+        cp -a bin/targets/x86/*/packages $kmodpkg_name
         rm -f $kmodpkg_name/Packages*
         cp -a bin/packages/x86_64/base/rtl88*a-firmware*.apk kmodpkg_name/ || true
         cp -a bin/packages/x86_64/base/natflow*.apk kmodpkg_name/ || true
