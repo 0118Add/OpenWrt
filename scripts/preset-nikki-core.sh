5b80db3b68be97dd27a8a0940963279cffa989b1
0118Add <60157610+0118Add@users.noreply.github.com>
2026-09-02 17:08:01 +0800

    Add files via upload

---
+++ scripts/preset-nikki-core.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#=================================================
+# File name: preset-nikki-core.sh
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: LovinYarn
+# github: https://github.com/xuanranran
+#=================================================
+mkdir -p files/etc/nikki/run/ui/metacubexd
+
+GEOIP_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat"
+GEOSITE_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat"
+GEOIP_METADB_URL="https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb"
+ASN_MMDB_URL="https://cdn.jsdelivr.net/gh/P3TERX/GeoLite.mmdb@download/GeoLite2-ASN.mmdb"
+
+wget -qO- $GEOIP_URL > files/etc/nikki/run/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/nikki/run/GeoSite.dat
+wget -qO- $GEOIP_METADB_URL > files/etc/nikki/run/geoip.metadb
+wget -qO- $ASN_MMDB_URL > files/etc/nikki/run/ASN.mmdb
+
+pushd files/etc/nikki/run/ui/
+curl -sSL https://codeload.github.com/haishanh/yacd/zip/refs/heads/gh-pages -o yacd-dist-cdn-fonts.zip
+curl -sSL https://github.com/MetaCubeX/metacubexd/releases/latest/download/compressed-dist.tgz -o compressed-dist.tgz
+curl -sSL https://github.com/Zephyruso/zashboard/archive/refs/heads/gh-pages-cdn-fonts.zip -o dist-cdn-fonts.zip
+tar zxf compressed-dist.tgz -C ./metacubexd
+unzip -q dist-cdn-fonts.zip && unzip -q yacd-dist-cdn-fonts.zip
+mv zashboard-gh-pages-cdn-fonts zashboard && mv yacd-gh-pages yacd
+rm -rf yacd-dist-cdn-fonts.zip dist-cdn-fonts.zip compressed-dist.tgz
+popd
+
+# mkdir -p files/etc/fchomo/
+# wget -qO- $GEOIP_URL > files/etc/fchomo/geoip.dat
+# wget -qO- $GEOSITE_URL > files/etc/fchomo/geosite.dat
+# wget -qO- $ASN_MMDB_URL > files/etc/fchomo/asn.mmdb
--- scripts/preset-terminal-tools.sh
+++ scripts/preset-terminal-tools.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
-
+set -e
+#=================================================
+# File name: preset-terminal-tools.sh
+# System Required: Linux
+# Version: 1.0
+# Lisence: MIT
+# Author: SuLingGG
+# Blog: https://mlapp.cn
+#=================================================
 mkdir -p files/root
 pushd files/root
 
+## Install oh-my-zsh
 # Clone oh-my-zsh repository
 git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh ./.oh-my-zsh
 
 # Install extra plugins
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
 
 # Get .zshrc dotfile
-cp $GITHUB_WORKSPACE/scripts/.zshrc .
+cp "$GITHUB_WORKSPACE/scripts/.zshrc" .
 
 popd
+
+# Preload oh-my-zsh completion cache after boot so SSH logins stay fast.
+mkdir -p files/etc/init.d files/etc/rc.d
+cat > files/etc/init.d/zsh-preload <<'EOF'
+#!/bin/sh /etc/rc.common
+
+START=99
+
+start() {
+	(
+		sleep 20
+		[ -x /usr/bin/zsh ] || exit 0
+		[ -r /root/.zshrc ] || exit 0
+		HOME=/root USER=root SHELL=/usr/bin/zsh /usr/bin/zsh -i -c exit >/tmp/zsh-preload.log 2>&1
+	) &
+}
+EOF
+chmod 755 files/etc/init.d/zsh-preload
+ln -sf ../init.d/zsh-preload files/etc/rc.d/S99zsh-preload
